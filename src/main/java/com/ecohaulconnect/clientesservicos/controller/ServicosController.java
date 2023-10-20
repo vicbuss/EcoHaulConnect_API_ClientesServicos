db0268da9cd478fa1a7061f9331fe5b2117a7dca
Victor Bussiki <victorbussiki@gmail.com>
2023-10-19 23:08:53 -0300

    feat (servico controller) adicionada rota de listagem de servicos com queries

--- src/main/java/com/ecohaulconnect/clientesservicos/controller/ServicosController.java
+++ src/main/java/com/ecohaulconnect/clientesservicos/controller/ServicosController.java
@@ -5,14 +5,16 @@ import com.ecohaulconnect.clientesservicos.domain.servico.DadosCadastroServico;
 import com.ecohaulconnect.clientesservicos.domain.servico.DadosListagemServico;
 import com.ecohaulconnect.clientesservicos.domain.servico.Servico;
 import com.ecohaulconnect.clientesservicos.domain.servico.ServicoRepository;
+import com.ecohaulconnect.clientesservicos.domain.transportador.TransportadorRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
@@ -23,6 +25,9 @@ public class ServicosController {
 
     @Autowired
     private ClienteRepository clienteRepository;
+
+    @Autowired
+    private TransportadorRepository transportadorRepository;
 
     @PostMapping
     @Transactional
@@ -39,5 +44,28 @@ public class ServicosController {
         var uri = uriBuilder.path("/servicos/{id}").buildAndExpand(servico.getId()).toUri();
 
         return ResponseEntity.created(uri).body(new DadosListagemServico(servico));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<DadosListagemServico>> listar(@PageableDefault(size = 10, sort = "dataAgendamento") Pageable paginacao,
+                                                             @RequestParam(value = "ativo", defaultValue = "true") boolean isAtivo,
+                                                             @RequestParam(value = "idCliente", required = false) Long idCliente,
+                                                             @RequestParam(value = "idTransportador", required = false) Long idTransportador)
+    {
+       Page<DadosListagemServico> page;
+       Specification<Servico> specification = Specification.where(((root, query, cb) -> cb.equal(root.get("ativo"), isAtivo)));
+
+       if(idCliente != null) {
+           var cliente = clienteRepository.getReferenceById(idCliente);
+           specification = specification.and(((root, query, cb) -> cb.equal(root.get("cliente"), cliente)));
+       }
+
+       if(idTransportador != null) {
+           var transportador = transportadorRepository.getReferenceById(idTransportador);
+           specification = specification.and(((root, query, cb) -> cb.equal(root.get("transportador"), transportador)));
+       }
+
+        page = servicoRepository.findAll(specification, paginacao).map(DadosListagemServico :: new);
+        return ResponseEntity.ok(page);
     }
 }
--- src/main/java/com/ecohaulconnect/clientesservicos/domain/servico/ServicoRepository.java
+++ src/main/java/com/ecohaulconnect/clientesservicos/domain/servico/ServicoRepository.java
@@ -1,6 +1,21 @@
 package com.ecohaulconnect.clientesservicos.domain.servico;
 
+import ch.qos.logback.core.testUtil.MockInitialContext;
+import com.ecohaulconnect.clientesservicos.domain.cliente.Cliente;
+import com.ecohaulconnect.clientesservicos.domain.transportador.Transportador;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Range;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ServicoRepository extends JpaRepository<Servico, Long> {
+    Page<Servico> findAllByAtivo(Pageable paginacao, boolean isAtivo);
+
+    Page<Servico> findAllByClienteAndAtivo(Pageable paginacao, Cliente Cliente, boolean isAtivo);
+
+    Page<DadosListagemServico> findAllByClienteAndTransportadorAndAtivo(Pageable paginacao, Cliente cliente,
+                                                                        Transportador transportador, boolean isAtivo);
+    Page<DadosListagemServico> findAllByTransportadorAndAtivo(Pageable paginacao, Transportador transportador, boolean isAtivo);
+    Page<Servico> findAll(Specification<Servico> specification, Pageable paginacao);
 }
